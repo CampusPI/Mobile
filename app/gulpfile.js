f47e88769a2d8cd066362eb615f3f1dcae6cb890
João Campinhos <joao.campinhos@gmail.com>
2014-05-11 13:43:36 +0100

    static server

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,9 +3,17 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  watch: [
+    'www/js/**/*.js',
+    'www/css/**/*.css',
+    'www/index.html',
+    'www/templates/**/*.html',
+    'www/img/**/*'
+  ]
 };
 
 gulp.task('sass', function(done) {
@@ -20,7 +28,31 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', function() {
+gulp.task('connect', function () {
+  var connect = require('connect');
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(connect.static('www'))
+    .use(connect.directory('www'));
+
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+        console.log('Started connect web server on http://localhost:9000');
+      });
+});
+
+gulp.task('serve', ['connect'], function () {
+  require('opn')('http://localhost:9000');
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+  var server = livereload();
+
+  gulp.watch(paths.watch).on('change', function (file) {
+    server.changed(file.path);
+  });
+
   gulp.watch(paths.sass, ['sass']);
 });
 
